c1d20289853adecd2ab5c91e5ee2a583ebeb14ec
1bouillonbaptiste <1bouillonbaptiste@gmail.com>
2024-08-18 12:34:41 +0200

    extract period code and test it

--- athena/apicultor/types/period.py
+++ athena/apicultor/types/period.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 UNITS = {
     "m": "minutes",
     "h": "hours",
@@ -13,17 +15,39 @@ class Period:
         value: int | None = None,
         unit: str | None = None,
     ):
-        if timeframe is None:
-            if value is None or unit is None:
-                raise ValueError(
-                    "`value` and `unit` must be given when `period` is `None`."
-                )
-            timeframe = str(value) + unit
-        else:
-            value = int(timeframe[:-1])
-            unit = str(timeframe[-1])
+        (self.timeframe, self.value, self.unit) = fill_missing_attributes(
+            timeframe, value, unit
+        )
+        self.unit_full = UNITS[self.unit]
 
-        self.timeframe = timeframe
-        self.value = value
-        self.unit = unit
-        self.unit_full = UNITS[unit]
+
+def fill_missing_attributes(
+    timeframe: str | None = None,
+    value: int | None = None,
+    unit: Literal["m", "h"] | None = None,
+):
+    """
+
+    Args:
+        timeframe: string containing both value and unit information
+        value: time unit number
+        unit: the time unit of the value
+
+    Returns:
+        same attributes with filled Nones
+
+    Raises:
+        ValueError: if timeframe is None and one of value / unit is None
+    """
+
+    if timeframe is None:
+        if value is None or unit is None:
+            raise ValueError(
+                "`value` and `unit` must be given when `period` is `None`."
+            )
+        timeframe = str(value) + unit
+    else:
+        value = int(timeframe[:-1])
+        unit = str(timeframe[-1])
+
+    return timeframe, value, unit
---
+++ tests/apicultor/types/test_period.py
@@ -0,0 +1,32 @@
+import pytest
+
+from athena.apicultor.types.period import fill_missing_attributes
+
+
+@pytest.mark.parametrize(
+    "timeframe, value, unit, expected_values",
+    [
+        ("4h", None, None, ("4h", 4, "h")),
+        ("30m", None, None, ("30m", 30, "m")),
+        (None, 1, "h", ("1h", 1, "h")),
+    ],
+)
+def test_fill_missing_attributes(timeframe, value, unit, expected_values):
+    assert fill_missing_attributes(timeframe, value, unit) == expected_values
+
+
+@pytest.mark.parametrize(
+    "timeframe, value, unit",
+    [
+        (
+            None,
+            None,
+            None,
+        ),
+        (None, 1, None),
+        (None, None, "h"),
+    ],
+)
+def test_fill_missing_attributes_fail(timeframe, value, unit):
+    with pytest.raises(ValueError):
+        fill_missing_attributes(timeframe, value, unit)
