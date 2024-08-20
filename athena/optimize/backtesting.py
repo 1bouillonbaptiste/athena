bb815acc3ada67885463a22094f63bfe27ebcc36
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-08-20 16:06:08 +0200

    get the trades of a strategy

---
+++ athena/optimize/backtesting.py
@@ -0,0 +1,8 @@
+from athena.optimize import Strategy
+import pandas as pd
+
+
+def get_trades_from_fluctuations_and_strategy(
+    fluctuations: pd.DataFrame, strategy: Strategy
+) -> list:
+    pass
