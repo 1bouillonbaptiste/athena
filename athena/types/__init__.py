4f5e3767c1d078cae4e673acb6026a46a7530f75
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-08-20 09:16:37 +0200

    new signal type

--- athena/types/__init__.py
+++ athena/types/__init__.py
@@ -1,4 +1,5 @@
 from athena.types.candle import Candle
 from athena.types.period import Period
+from athena.types.signal import Signal
 
-__all__ = ["Candle", "Period"]
+__all__ = ["Candle", "Period", "Signal"]
