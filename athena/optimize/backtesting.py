3b229a58f6f7006772948a7972a8b65aef2a8553
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-08-20 21:32:55 +0200

    update backtetsing `get_trades_from_strategy_and_fluctuations` signature

--- athena/optimize/backtesting.py
+++ athena/optimize/backtesting.py
@@ -1,8 +1,17 @@
-from athena.optimize import Strategy
+from athena.optimize import Strategy, Trade
 import pandas as pd
 
 
-def get_trades_from_fluctuations_and_strategy(
-    fluctuations: pd.DataFrame, strategy: Strategy
-) -> list:
-    pass
+def get_trades_from_strategy_and_fluctuations(
+    strategy: Strategy, fluctuations: pd.DataFrame
+) -> list[Trade]:
+    """Compute the trades that a strategy would have made given fluctuations.
+
+    Args:
+        strategy: the strategy to get entry signals
+        fluctuations: financial data
+
+    Returns:
+        market movement as a list of trades
+    """
+    return []
