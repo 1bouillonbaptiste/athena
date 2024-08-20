261a5101dddf2ae6db8bcae22bc54d535113ea69
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-08-20 21:39:07 +0200

    todos

--- athena/optimize/backtesting.py
+++ athena/optimize/backtesting.py
@@ -14,4 +14,9 @@ def get_trades_from_strategy_and_fluctuations(
     Returns:
         market movement as a list of trades
     """
+    # TODO: iterate over fluctuations
+    #       1/ get entry signal
+    #       2/ when buy, open a position at close price (= beginning of next candle)
+    #       3/ check exit conditions (strategy sell, tp or sl)
+    #       4/ close position & create new trade
     return []
