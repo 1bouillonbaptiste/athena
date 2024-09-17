d7d767af775e03a5747bba90cd6f1c929805e7b8
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-09-17 17:50:27 +0200

    better test

--- tests/fluctuations/test_fluctuations.py
+++ tests/fluctuations/test_fluctuations.py
@@ -7,48 +7,44 @@ from athena.core.interfaces.fluctuations import sanitize_candles
 
 
 @pytest.fixture
-def candle():
-    return [
-        Candle(
-            volume=0,
-            open=100,
-            close=100,
-            high=150,
-            low=70,
-            coin=Coin.BTC,
-            currency=Coin.USDT,
-            period="1h",
-            open_time=datetime.datetime(2024, 1, 1),
-            close_time=datetime.datetime(2024, 1, 2),
-            quote_volume=150,
-            nb_trades=100,
-            taker_volume=50,
-            taker_quote_volume=70,
-        ),
-        # volume at 100
-        Candle(
-            volume=100,
-            open=100,
-            close=100,
-            high=150,
-            low=70,
-            coin=Coin.BTC,
-            currency=Coin.USDT,
-            period="1h",
-            open_time=datetime.datetime(2024, 1, 1),
-            close_time=datetime.datetime(2024, 1, 2),
-            quote_volume=150,
-            nb_trades=100,
-            taker_volume=50,
-            taker_quote_volume=70,
-        ),
-    ]
+def candles():
+    other_params = {
+        "open": 100,
+        "close": 100,
+        "high": 150,
+        "low": 70,
+        "coin": Coin.BTC,
+        "currency": Coin.USDT,
+        "period": "1h",
+        "open_time": datetime.datetime(2024, 1, 1),
+        "close_time": datetime.datetime(2024, 1, 2),
+        "quote_volume": 150,
+        "nb_trades": 100,
+        "taker_volume": 50,
+        "taker_quote_volume": 70,
+    }
+
+    def _candles(volumes: list[int]) -> list[Candle]:
+        return [
+            Candle(
+                volume=volume,
+                **other_params,
+            )
+            for volume in volumes
+        ]
+
+    return _candles
 
 
-def test_sanitize_candles(candle):
-    """all candle's volume not > 0."""
+def test_sanitize_candles(candles):
+    """Remove invalid candles."""
 
-    sanitized_candles = sanitize_candles(candle)
+    sanitized_candles = sanitize_candles(candles(volumes=[50, 60, 0, 100, 0]))
 
-    assert len(sanitized_candles) == 1
-    assert sanitized_candles[0].volume == 100
+    assert [candle.volume for candle in sanitized_candles] == [50, 60, 100]
+
+
+def test_sanitize_candles_empty():
+    sanitized_candles = sanitize_candles([])
+
+    assert sanitized_candles == []
