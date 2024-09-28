9061d46ee35f4f4f7a3308c74b81960809a781b3
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-09-28 17:43:12 +0200

    get_strategy

---
+++ athena/tradingtools/strategies/__init__.py
@@ -0,0 +1,10 @@
+from typing import Any
+
+from athena.tradingtools.strategies.dca import StrategyDCA
+
+STRATEGIES_MATCH = {"strategy_dca": StrategyDCA}
+
+
+def get_strategy(strategy_name: str, strategy_params: dict[str:Any]):
+    """Build a Strategy object."""
+    return STRATEGIES_MATCH[strategy_name](**strategy_params)
