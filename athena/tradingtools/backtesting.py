e7bef1e846d8d4cdf6fb7fe5b1fa673ec181f4e7
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-09-30 17:24:20 +0200

    open position only if money is available

--- athena/tradingtools/backtesting.py
+++ athena/tradingtools/backtesting.py
@@ -124,27 +124,28 @@ def get_trades_from_strategy_and_fluctuations(
             position = None
 
         if signal == Signal.BUY and position is None:
-            position = Position.open(
-                strategy_name=strategy.name,
-                coin=traded_coin,
-                currency=currency,
-                open_date=candle.close_time,
-                open_price=candle.close,
-                money_to_invest=portfolio.get_available(currency)
-                * strategy.position_size,
-                stop_loss=candle.close * (1 - strategy.stop_loss_pct)
-                if strategy.stop_loss_pct is not None
-                else 0,
-                take_profit=candle.close * (1 + strategy.take_profit_pct)
-                if strategy.take_profit_pct is not None
-                else float("inf"),
-            )
-            portfolio.update_coin_amount(
-                coin=currency, amount_to_add=-position.initial_investment
-            )
-            portfolio.update_coin_amount(
-                coin=traded_coin, amount_to_add=position.amount
-            )
+            money_to_invest = portfolio.get_available(currency) * strategy.position_size
+            if portfolio.get_available(currency) > money_to_invest:
+                position = Position.open(
+                    strategy_name=strategy.name,
+                    coin=traded_coin,
+                    currency=currency,
+                    open_date=candle.close_time,
+                    open_price=candle.close,
+                    money_to_invest=money_to_invest,
+                    stop_loss=candle.close * (1 - strategy.stop_loss_pct)
+                    if strategy.stop_loss_pct is not None
+                    else 0,
+                    take_profit=candle.close * (1 + strategy.take_profit_pct)
+                    if strategy.take_profit_pct is not None
+                    else float("inf"),
+                )
+                portfolio.update_coin_amount(
+                    coin=currency, amount_to_add=-position.initial_investment
+                )
+                portfolio.update_coin_amount(
+                    coin=traded_coin, amount_to_add=position.amount
+                )
         elif signal == Signal.SELL and position is not None:
             position.close(
                 close_date=candle.close_time,
