bd0401685f33aaa68b9fa06fd676271a82137e2b
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-10-02 18:58:44 +0200

    gain 0.3s on backtest entrypoint test

--- tests/entrypoints/test_backtest.py
+++ tests/entrypoints/test_backtest.py
@@ -17,7 +17,7 @@ def config():
             "currency": "USDT",
             "period": "1h",
             "from_date": "2020-01-01",
-            "to_date": "2020-01-03",
+            "to_date": "2020-01-02",
         },
         "strategy": {
             "name": "strategy_dca",
@@ -32,15 +32,20 @@ def config():
     }
 
 
-def test_run_backtest(config, generate_candles, tmp_path):
+def test_run_backtest(config, generate_candles, tmp_path, mocker):
     config_path = tmp_path / "config.yaml"
     root_dir = tmp_path / "raw_market_data"
     output_dir = tmp_path / "results"
 
     config_path.write_text(json.dumps(config))
 
+    mocker.patch(
+        "athena.performance.report._plot_trades_on_fluctuations",
+        return_value="",
+    )
+
     start_date = datetime.datetime(2020, 1, 1)
-    for day_ii in range(3):
+    for day_ii in range(2):
         Fluctuations.from_candles(
             generate_candles(
                 from_date=start_date + datetime.timedelta(days=day_ii),
