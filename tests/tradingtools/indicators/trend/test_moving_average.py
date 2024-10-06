d69f1dc4704d974b781a97d73aa9af847e390d70
Baptiste Bouillon <baptiste.bouillon@preligens.com>
2024-10-06 08:54:06 +0200

    moving average indicators

---
+++ tests/tradingtools/indicators/trend/test_moving_average.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pytest
+
+from athena.tradingtools.indicators.trend.moving_average import (
+    simple_moving_average,
+    exponential_moving_average,
+)
+
+
+def test_simple_moving_average(input_data):
+    assert np.allclose(simple_moving_average(input_data, window_size=1), input_data)
+    assert np.allclose(
+        simple_moving_average(input_data, window_size=2), [1.5, 1.5, 1.5, 1.5, 2.5, 2]
+    )
+    assert np.allclose(
+        simple_moving_average(input_data, window_size=len(input_data)),
+        np.repeat(np.mean(input_data), len(input_data)),
+    )
+
+
+def test_simple_moving_average_fails(input_data):
+    with pytest.raises(ValueError):
+        assert simple_moving_average(input_data, window_size=0)
+
+
+def test_exponential_moving_average(input_data):
+    assert np.allclose(
+        exponential_moving_average(input_data, window_size=1), input_data
+    )
+    assert np.allclose(
+        exponential_moving_average(input_data, window_size=3),
+        [1.25, 1.25, 1.25, 1.625, 2.3125, 1.65625],
+    )
+    assert np.allclose(
+        exponential_moving_average(input_data, window_size=len(input_data)),
+        np.repeat(1.62830963, len(input_data)),
+    )
+
+
+def test_exponential_moving_average_fails(input_data):
+    with pytest.raises(ValueError):
+        assert exponential_moving_average(input_data, window_size=0)
